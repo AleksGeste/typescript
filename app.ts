ec8992802b4f9cf980ab94deb5eba264dd32c285
Aleksandrs Čikiņovs <a.cikinovs@icloud.com>
2021-12-21 00:03:24 +0200

    objects and arrays

--- app.ts
+++ app.ts
@@ -1,1 +1,23 @@
-console.log("Hey!!!");
+// objects
+// object, array
+
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: "Aleks",
+  age: 38,
+  hobbies: ["Sport", "Music"],
+};
+
+let favActivities: string[];
+favActivities = ["coffie", "salat"];
+// let favActivities: any[];
+// favActivities = ['coffie', 'salat', 1, false]
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toLocaleUpperCase());
+}
---
+++ basics.ts
@@ -0,0 +1,38 @@
+console.log("Core Typescript Types");
+
+// Core primitive types
+
+// numbers, string, boolean
+
+// func which take parameters with types
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  // check type of number1
+  console.log("Type of n1: ", typeof n1);
+
+  // javascrip approach, which is not good
+  // if (typeof n1 !== "number" || typeof n2 !== "number") {
+  //   throw new Error("Incorrect input!");
+  // }
+
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+// const number1 = 5;
+let number1: number;
+number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+// const result = add(number1, number2, printResult, resultPhrase);
+// console.log(result);
+
+add(number1, number2, printResult, resultPhrase);
+
+// string
+const str = "Hello world";
